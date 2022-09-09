e84f612ba3905d8c4ea897ef2c5045ad61ff4195
maxarrowood <112727066+maxarrowood@users.noreply.github.com>
2022-09-09 19:18:35 +0000

    update

---
+++ prog52a.py
@@ -0,0 +1,11 @@
+length = int(input("Please Enter Length: "))
+width = int(input("Please Enter Width: "))
+
+area = length * width
+perim = 2 * length + 2 * width
+
+print("The length is", length)
+print("The width is", width)
+
+print("\nThe area is", area)
+print("The area is", perim)
