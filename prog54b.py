28f3a8a1c00af81ed742d801b90b40126351a6ac
maxarrowood <112727066+maxarrowood@users.noreply.github.com>
2022-09-09 19:36:38 +0000

    update

---
+++ prog54b.py
@@ -0,0 +1,10 @@
+number1 = float(input("Number 1: "))
+number2 = float(input("Number 2: "))
+number3 = float(input("Number 3: "))
+number4 = float(input("Number 4: "))
+
+sum = number1 + number2 + number3 + number4
+avg = sum / 4
+
+print("\nThe Sum is", sum)
+print("\nThe Average is", avg)
---
+++ prog54c.py
@@ -0,0 +1,7 @@
+rad = float(input("Radius: "))
+
+circum = 2 * 3.14159 * rad
+area = 3.14159 * (rad) ^ 2
+
+print("\nCircumference is", circum)
+print("\nArea is", area)
