92102099f0f3164111533f8b2d8261a8a6906e30
maxarrowood <112727066+maxarrowood@users.noreply.github.com>
2022-09-11 23:13:36 +0000

    update

--- prog54b.py
+++ prog54b.py
@@ -3,8 +3,8 @@ number2 = float(input("Number 2: "))
 number3 = float(input("Number 3: "))
 number4 = float(input("Number 4: "))
 
-sum = number1 + number2 + number3 + number4
-avg = sum / 4
+sum_ = number1 + number2 + number3 + number4
+avg = sum_ / 4
 
-print("\nThe Sum is", sum)
+print("\nThe Sum is", sum_)
 print("\nThe Average is", avg)
