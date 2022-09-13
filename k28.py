c6b982f121e205235d506e2887ab556f8304cad0
maxarrowood <112727066+maxarrowood@users.noreply.github.com>
2022-09-13 19:22:53 +0000

    k28

---
+++ k28.py
@@ -0,0 +1,43 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
