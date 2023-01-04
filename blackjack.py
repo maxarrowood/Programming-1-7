0f79176aff4e8e0bd7b4399ff5274af371805f5d
maxarrowood <112727066+maxarrowood@users.noreply.github.com>
2023-01-04 14:55:31 +0000

    update

--- blackjack.py
+++ blackjack.py
@@ -1,9 +1,20 @@
-from tkinter import *
+import pygame
 
-# setup
-top = Tk()
-top.geometry("600x450")
-name = Label(top, text="Blackjack").place(x=300, y=100)
-starbutton = Button(top, text="Start", fg="red")
+black = 0, 0, 0
+white = 255, 255, 255
+blue = 53, 69, 148
+green = 72, 112, 70
+red = 173, 39, 36
 
-top.mainloop()
+pygame.init()
+width, height = 800, 600
+backgroundColor = 72, 112, 70
+screen = pygame.display.set_mode((width, height))
+logo = pygame.image.load("cards.png").convert()
+pygame.display.set_caption("Max's Blackjack")
+
+while True:
+    screen.fill(backgroundColor)
+    pygame.draw.rect(screen, red, pygame.Rect(325, 400, 150, 60))
+
+    pygame.display.flip()
