3c937226a28387c4dcf4810ec92969926295fbb5
maxarrowood <112727066+maxarrowood@users.noreply.github.com>
2023-01-10 20:35:18 +0000

    update

--- blackjack.py
+++ blackjack.py
@@ -1,16 +1,17 @@
 import pygame
+import os
 
 black = 0, 0, 0
 white = 255, 255, 255
 blue = 53, 69, 148
-green = 72, 112, 70
+green = 85, 164, 77
 red = 173, 39, 36
 
 pygame.init()
 width, height = 800, 600
 backgroundColor = 72, 112, 70
 screen = pygame.display.set_mode((width, height))
-logo = pygame.image.load("cards.png").convert()
+logo = pygame.image.load("maxlogo.png").convert()
 pygame.display.set_caption("Max's Blackjack")
 
 #starting page
@@ -24,13 +25,14 @@ def main():
     while True:
         pygame.init()
 
-        pygame.draw.rect(screen, red, pygame.Rect(200, 400, 400, 60))
-        screen.blit(text_surface, (225, 415))
+        screen.blit(logo, (0, 0))
         pygame.display.flip()
 
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
+                os.system('clear')
                 screen.fill(red)
+                pygame.display.update()
 
         for event in pygame.event.get():
             if pygame.K_ESCAPE:
